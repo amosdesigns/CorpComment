aa6db01dd5579114f8bd4c2165b3299974ff3896
amosdesigns <mail@amosdesigns.net>
2024-05-08 20:39:23 -0400

    date formating
    Signed-off-by: amosdesigns <mail@amosdesigns.net>

--- src/components/feedback/items/FBIDaysAgo.tsx
+++ src/components/feedback/items/FBIDaysAgo.tsx
@@ -1,7 +1,11 @@
 import {type FBIDaysAgoProps } from '../../../lib/types';
 
 const FBIDaysAgo = ({ daysAgo }: FBIDaysAgoProps) => {
-  return <p>{daysAgo}d</p>;
+  return (
+    <p className={(daysAgo === 0) ? 'u-bold':""}>
+      {daysAgo === 0 ? "NEW" : `${daysAgo}d`}
+    </p>
+  );
 };
 
 export default FBIDaysAgo;
